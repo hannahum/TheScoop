1c898874112abbf137c32a32bbfccc9a64446ed4
hannahum <149267982+hannahum@users.noreply.github.com>
2023-12-05 20:39:36 -0800

    Add files via upload

--- pages/About 💡.py
+++ pages/About 💡.py
@@ -1,1 +1,16 @@
+import streamlit as st
+
+title = st.title("What's The Scoop? 💡")
+st.markdown(
+    """
+    **Welcome!** \n
+    We're glad you're here! \n 
+    The Scoop is an ice cream recommender that can help you discover new ice cream flavors! Tell us your current cravings, dietary restrictions, or select our random feature to find a scoop just right for you! \n
+    **Featured Brands:**
+"""
+)
+st.image("hd_logo.png")
+st.image("talenti_logo.png")
+st.image("breyers_logo.png")
+st.image("bj_logo.jpeg")
 
