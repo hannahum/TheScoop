3a99bde08dfd52e3828b87aabdc82cac38a8b428
hannahum <149267982+hannahum@users.noreply.github.com>
2023-12-05 20:38:25 -0800

    Add files via upload

---
+++ pages/Catalog 🗂️.py
@@ -0,0 +1,342 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import random
+
+df = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/products.csv")
+bj = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/bj/bj_products.csv")
+breyers = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/breyers/breyers_products.csv")
+hd = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/hd/hd_products.csv")
+talenti = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/talenti/talenti_products.csv")
+reviews = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/reviews.csv")
+reviews['title'] = reviews['title'].fillna('')
+
+title = st.title('Catalog 🗂️')
+
+def button_control(button_key):
+    if button_key not in st.session_state:
+        st.session_state[button_key] = False
+    return st.session_state[button_key]
+
+def display_ingredients(flavor_index, df):
+    ingredients = df['ingredients'].iloc[flavor_index]
+    st.write(ingredients)
+
+def default_list():
+    st.subheader("Explore All")
+    indices_list = []
+    for i in range(len(df)):
+        indices_list.append(i)
+    shuffled_flavors = pd.Series(indices_list).sample(frac=1).reset_index(drop=True)
+    for i in range(len(df)):
+        if (i == 0):
+            st.write("\n \n")
+        name = df['name'].iloc[shuffled_flavors[i]]
+        st.markdown(f"**__{name}__**", unsafe_allow_html=True)
+        key = df['key'].iloc[shuffled_flavors[i]]
+        image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+        image = Image.open(image_path)
+        st.image(image)
+
+        brand = df['brand'].iloc[shuffled_flavors[i]]
+        if (brand == "hd"):
+            st.write("**Brand:** Häagen-Dazs")
+        elif (brand == "bj"):
+            st.write("**Brand:** Ben & Jerry's")
+        elif (brand == "talenti"):
+            st.write("**Brand:** Talenti")
+        elif (brand == "breyers"):
+            st.write("**Brand:** Breyers")
+
+        rating = df['rating'].iloc[shuffled_flavors[i]]
+        st.write(f"**Rating:** {rating}")
+
+        description = df['description'].iloc[shuffled_flavors[i]]
+        st.write(f'**Description:** {description}')
+
+        ingredients = df['ingredients'].iloc[shuffled_flavors[i]]
+        st.write(f'**Ingredients:** {ingredients}')
+
+        st.write(f'**Reviews:** \n')
+        rev_authors = []
+        rev_dates = []
+        rev_titles = []
+        rev_text = []
+        for j in range(len(reviews)):
+            if (reviews['key'][j] == key):
+                rev_authors.append(reviews['author'].iloc[j])
+                rev_dates.append(reviews['date'].iloc[j])
+                rev_titles.append(reviews['title'].iloc[j])
+                rev_text.append(reviews['text'].iloc[j])
+        random_indices = random.sample(range(len(rev_authors)), 3)
+        for k in range(len(random_indices)):
+            st.write(f'**User:** {rev_authors[random_indices[k]]}')
+            st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+            st.write(f'_{rev_titles[random_indices[k]]}_')
+            st.write(f'"{rev_text[random_indices[k]]}"')
+            st.write("\n \n")
+    st.write("\n \n")
+    st.write("\n \n")
+    st.write("\n \n")
+
+def brand_page():
+    st.subheader("Explore Flavors by Brand")
+    bj_button = st.button("Ben & Jerry's")
+    breyers_button = st.button("Breyers")
+    hd_button = st.button("Häagen-Dazs")
+    talenti_button = st.button("Talenti")
+    st.write("\n \n")
+    if (bj_button):
+        st.subheader("Ben & Jerry's")
+        for i in range(len(bj)):
+            if (i == 0):
+                st.write("\n \n")
+            name = bj['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = bj['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/bj/bj_images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            rating = bj['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = bj['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+    if(breyers_button):
+        st.subheader("Breyers")
+        for i in range(len(breyers)):
+            name = breyers['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = breyers['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/breyers/breyers_images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            rating = breyers['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = breyers['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+    if(hd_button):
+        st.subheader("Häagen-Dazs")
+        for i in range(len(hd)):
+            name = hd['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = hd['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/hd/hd_images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            rating = hd['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = hd['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+            st.write("\n \n")
+            st.write("\n \n")
+    if(talenti_button):
+        st.subheader("Talenti")
+        for i in range(len(talenti)):
+            name = talenti['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = talenti['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/talenti/talenti_images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            rating = talenti['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = talenti['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+
+def rating_page():
+    st.subheader("Explore Flavors by Rating")
+    highest_button = st.button("Highest Rated")
+    lowest_button = st.button("Lowest Rated")
+    st.write("\n \n")
+    st.write("\n \n")
+    if (highest_button):
+        sorted_indices = df.sort_values(by='rating', ascending=False)
+        for i in range(len(sorted_indices)):
+            name = sorted_indices['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = sorted_indices['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            brand = sorted_indices['brand'].iloc[i]
+            if (brand == "hd"):
+                st.write("**Brand:** Häagen-Dazs")
+            elif (brand == "bj"):
+                st.write("**Brand:** Ben & Jerry's")
+            elif (brand == "talenti"):
+                st.write("**Brand:** Talenti")
+            elif (brand == "breyers"):
+                st.write("**Brand:** Breyers")
+
+            rating = sorted_indices['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = sorted_indices['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+    if (lowest_button):
+        sorted_indices = df.sort_values(by='rating', ascending=True)
+        for i in range(len(sorted_indices)):
+            name = sorted_indices['name'].iloc[i]
+            st.write(f"**{name}**")
+            key = sorted_indices['key'].iloc[i]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            brand = sorted_indices['brand'].iloc[i]
+            if (brand == "hd"):
+                st.write("**Brand:** Häagen-Dazs")
+            elif (brand == "bj"):
+                st.write("**Brand:** Ben & Jerry's")
+            elif (brand == "talenti"):
+                st.write("**Brand:** Talenti")
+            elif (brand == "breyers"):
+                st.write("**Brand:** Breyers")
+
+            rating = sorted_indices['rating'].iloc[i]
+            st.write(f"**Rating:** {rating}")
+            description = sorted_indices['description'].iloc[i]
+            st.write(f'**Description:** {description}')
+            ingredients = df['ingredients'].iloc[i]
+            st.write(f'**Ingredients:** {ingredients}')
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            if (len(rev_authors) >= 3):
+                random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+        st.write("\n \n")
+
+page = st.sidebar.radio("Ice Cream Products Sorted By:", ("All 🌎", "Brand 📍", "Rating ⭐"))
+# Display selected page
+if page == "All 🌎":
+    default_list()
+elif page == "Brand 📍":
+    brand_page()
+elif page == "Rating ⭐":
+    rating_page()
---
+++ pages/Ice Cream Recommendation 🍦.py
@@ -0,0 +1,211 @@
+import streamlit as st
+import Recommender
+from PIL import Image
+import pandas as pd
+import random
+
+df = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/products.csv")
+reviews = pd.read_csv("/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/reviews.csv")
+reviews['title'] = reviews['title'].fillna('')
+
+title = st.title('Find Your Scoop 🔍')
+st.markdown(
+    """
+    Choose one of the categories to start your search for the scoop just right for you!  
+"""
+)
+st.image('random.png')
+
+def button_control(button_key):
+    if button_key not in st.session_state:
+        st.session_state[button_key] = False
+    return st.session_state[button_key]
+
+def cravings_page():
+    st.title("Recommendations by Cravings 💭")
+    st.markdown(
+        """
+        Welcome to Recommendations by Cravings! \n
+        Enter your latest cravings below and we'll provide you with ice cream flavors tailored to your taste!
+    """
+    )
+    user_input = st.text_input('')
+    if user_input:
+        recs = Recommender.craving_recommendations(user_input)
+        for i in range(len(recs)):
+            if (i == 0):
+                st.write("\n \n")
+                st.write("\n \n")
+            name = df['name'].iloc[recs[i]]
+            st.write(f"**{name}**")
+            key = df['key'].iloc[recs[i]]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+            brand = df['brand'].iloc[recs[i]]
+            if (brand == "hd"):
+                st.write("**Brand:** Häagen-Dazs")
+            elif (brand == "bj"):
+                st.write("**Brand:** Ben & Jerry's")
+            elif (brand == "talenti"):
+                st.write("**Brand:** Talenti")
+            elif (brand == "breyers"):
+                st.write("**Brand:** Breyers")
+
+            rating = df['rating'].iloc[recs[i]]
+            st.write(f"**Rating:** {rating}")
+
+            description = df['description'].iloc[recs[i]]
+            st.write(f'**Description:** {description}')
+
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+
+            review_crav_button_state = button_control('review_crav_button')
+            review_crav_button = st.button('View Reviews', key=rev_authors)
+            if review_crav_button:
+                review_crav_button_state = not review_crav_button_state
+            if review_crav_button_state:
+                random_indices = random.sample(range(len(rev_authors)), 8)
+                for k in range(len(random_indices)):
+                    st.write("\n \n")
+                    st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                    st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                    st.write(f'_{rev_titles[random_indices[k]]}_')
+                    st.write(f'"{rev_text[random_indices[k]]}"')
+                    st.write("\n \n")
+                review_crav_button_state = st.empty()
+                review_crav_button = review_crav_button_state.button('Hide Reviews')
+            ingredients = df['ingredients'].iloc[recs[i]]
+            ingred_crav_button_state = button_control('ingred_crav_button')
+            ingred_crav_button = st.button('View Ingredients', key=recs[i])
+            if ingred_crav_button:
+                ingred_crav_button_state = not ingred_crav_button_state
+            if ingred_crav_button_state:
+                st.write(ingredients)
+            st.write("\n \n")
+            st.write("\n \n")
+
+def diet_restrictions_page():
+    st.title("Recommendations by Dietary Restrictions 📋")
+    st.markdown(
+        """
+        Welcome to Recommendations by Dietary Restrictions! \n
+        Enter your dietary restrictions below and we'll find the perfect scoop for you!
+    """
+    )
+    df['ingredients'] = df['ingredients'].apply(lambda x: [ingredient.strip() for ingredient in x.split(',')])
+    selected_options = st.multiselect('Select Dietary Restrictions:', df['ingredients'].explode().unique())
+
+    with st.expander("Filtered Flavors"):
+        filtered_flavors = Recommender.display_filtered_flavors(df, selected_options)
+        if filtered_flavors is not None and not filtered_flavors.empty:
+            for index, row in filtered_flavors.iterrows():
+                name = row['name']
+                st.subheader(f"**{name}**")
+                key = row['key']
+                image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+                image = Image.open(image_path)
+                st.image(image)
+
+                brand = row['brand']
+                if (brand == "hd"):
+                    st.write("**Brand:** Häagen-Dazs")
+                elif (brand == "bj"):
+                    st.write("**Brand:** Ben & Jerry's")
+                elif (brand == "talenti"):
+                    st.write("**Brand:** Talenti")
+                elif (brand == "breyers"):
+                    st.write("**Brand:** Breyers")
+
+                rating = row['rating']
+                st.write(f"**Rating:** {rating}")
+
+                description = row['description']
+                st.write(f'**Description:** {description}')
+
+                ingredients = row['ingredients']
+                st.write(f'**Ingredients:** {ingredients}')
+        else:
+            st.write("No matching flavors found.")
+
+def random_page():
+    st.title("Random Recommendations 🎲")
+    st.markdown(
+        """
+        Welcome to Random Recommendations! \n
+        Click 'Generate' to explore new flavors!
+    """
+    )
+    generate = st.button('Generate')
+    if generate:
+        recs = Recommender.random_recommendations()
+        for i in range(len(recs)):
+            if (i == 0):
+                st.write("\n \n")
+                st.write("\n \n")
+            name = df['name'].iloc[recs[i]]
+            st.write(f"**{name}**")
+            key = df['key'].iloc[recs[i]]
+            image_path = f"/Users/hannahum/PycharmProjects/The Scoop/ice cream dataset/combined/images/{key}.png"
+            image = Image.open(image_path)
+            st.image(image)
+
+            brand = df['brand'].iloc[recs[i]]
+            if (brand == "hd"):
+                st.write(f'**Brand:** Häagen-Dazs')
+            elif (brand == "bj"):
+                st.write(f"**Brand:** Ben & Jerry's")
+            elif (brand == "talenti"):
+                st.write(f'**Brand:** Talenti')
+            elif (brand == "breyers"):
+                st.write(f'**Brand:** Breyers')
+
+            rating = df['rating'].iloc[recs[i]]
+            st.write(f"**Rating:** {rating}")
+
+            description = df['description'].iloc[recs[i]]
+            st.write(description)
+
+            ingredients = df['ingredients'].iloc[recs[i]]
+            st.write(f'**Ingredients:** {ingredients}')
+
+            st.write(f'**Reviews:** \n')
+            rev_authors = []
+            rev_dates = []
+            rev_titles = []
+            rev_text = []
+            for j in range(len(reviews)):
+                if (reviews['key'][j] == key):
+                    rev_authors.append(reviews['author'].iloc[j])
+                    rev_dates.append(reviews['date'].iloc[j])
+                    rev_titles.append(reviews['title'].iloc[j])
+                    rev_text.append(reviews['text'].iloc[j])
+            random_indices = random.sample(range(len(rev_authors)), 3)
+            for k in range(len(random_indices)):
+                st.write(f'**User:** {rev_authors[random_indices[k]]}')
+                st.write(f'**Date:** {rev_dates[random_indices[k]]}')
+                st.write(f'_{rev_titles[random_indices[k]]}_')
+                st.write(f'"{rev_text[random_indices[k]]}"')
+                st.write("\n \n")
+            st.write("\n \n")
+            st.write("\n \n")
+
+# Sidebar navigation
+page = st.sidebar.radio("Find Your Scoop Based On:", ("Cravings 💭", "Dietary Restrictions 📋", "Random 🎲"))
+
+# Display selected page
+if page == "Cravings 💭":
+    cravings_page()
+elif page == "Dietary Restrictions 📋":
+    diet_restrictions_page()
+elif page == "Random 🎲":
+    random_page()
